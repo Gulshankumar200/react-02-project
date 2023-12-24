3439f71089d70498aa84c8b1aaabb14e4789b32c
Gulshan Kumar <gk1647125@gmail.com>
2023-12-24 15:59:25 +0530

    first time set up the project

---
+++ vite-o2/src/App.jsx
@@ -0,0 +1,18 @@
+
+
+import './App.css'
+import Card from'./components/card'
+
+function App() {
+  
+
+  return (
+    <>
+      <h1 className='bg-green-400 text-black'>hai i am learnning with react</h1>
+      <Card username="gulshan" btnText="click me"/>
+      <Card username="Every one" btnText="fellow me"/>
+    </>
+  )
+}
+
+export default App
